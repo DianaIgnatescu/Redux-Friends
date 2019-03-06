14c1c48e382922710741368f4644053c7ffa265a
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-06 18:36:45 +0000

    MVP: Created action types

---
+++ friends/src/actions/index.js
@@ -0,0 +1,31 @@
+export const FETCH_FRIENDS = 'FETCHING_FRIENDS';
+export const FETCH_FRIENDS_SUCCESS = 'FETCH_FRIENDS_SUCCESS';
+export const FETCH_FRIENDS_FAILURE = 'FETCH_FRIENDS_FAILURE';
+export const ADD_FRIEND = 'ADD_FRIEND';
+export const ADD_FRIEND_SUCCESS = 'ADD_FRIEND_SUCCESS';
+export const ADD_FRIEND_FAILURE = 'ADD_FRIEND_FAILURE';
+export const DELETE_FRIEND = 'DELETE_FRIEND';
+export const DELETE_FRIEND_SUCCESS = 'DELETE_FRIEND_SUCCESS';
+export const DELETE_FRIEND_FAILURE = 'DELETE_FRIEND_FAILURE';
+export const UPDATE_FRIEND = 'UPDATE_FRIEND';
+export const UPDATE_FRIEND_SUCCESS = 'UPDATE_FRIEND_SUCCESS';
+export const UPDATE_FRIEND_FAILURE = 'UPDATE_FRIEND_FAILURE';
+export const SAVE_FRIEND = 'UPDATE_FRIEND';
+export const SAVE_FRIEND_SUCCESS = 'UPDATE_FRIEND_SUCCESS';
+export const SAVE_FRIEND_FAILURE = 'UPDATE_FRIEND_FAILURE';
+
+// export const getFriends = () => {
+
+// }
+
+// export const addFriend = () => {
+
+// }
+
+// export const deleteFriend = () => {
+  
+// }
+
+// export const updateFriend = () => {
+  
+// }
