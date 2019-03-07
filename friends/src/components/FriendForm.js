f9c1936d41e1063be106a4dd4a91966ed174376d
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-07 15:27:43 +0000

    MVP: Fixed up FriendForm component

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,77 +1,55 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import { connect } from 'react-redux';
-import { addFriend } from '../actions';
 
-const clearForm = (target) => {
-  target.childNodes.forEach((child) => {
-    if (child.value) {
-      child.value = '';
+const handleClick = (addFriend, fetchFriends) => (event) => {
+  event.preventDefault();
+  console.log('clicked');
+  let age = '';
+  let email = '';
+  let name = '';
+  event.target.parentNode.childNodes.forEach((childNode) => {
+    if (childNode.name === 'name') {
+      name = childNode.value;
+    } else if (childNode.name === 'age') {
+      age = childNode.value;
+    } else if (childNode.name === 'email') {
+      email = childNode.value;
     }
   });
-};
-
-const handleSubmit = (event, addFriend) => {
-  event.preventDefault();
-  addFriend();
-  clearForm(event.target.parentNode);
-};
-
-class FriendForm extends React.Component {
-  nameRef = React.createRef()
-  ageRef = React.createRef()
-  emailRef = React.createRef()
-
-  onAddFriend = () => {
-    const nameRef = this.nameRef.current.value;
-    const ageRef = this.ageRef.current.value;
-    const emailRef = this.emailRef.current.value;
-    this.props.addFriend(nameRef, ageRef, emailRef);
+  console.log(name, age, email);
+  if (name && age && email) {
+    addFriend(name, age, email);
+    fetchFriends();
   }
+};
 
-  render() {
-    return (
-      <div className="form-container">
-        <h2>Add a New Friend</h2>
-        <form className="friends-form">
-          <input
-            name="name"
-            type="text"
-            placeholder="Name..."
-            ref={this.nameRef}
-          />
-          <input
-            name="age"
-            type="number"  
-            placeholder="Age..."
-            ref={this.ageRef}
-          />
-          <input
-            name="email"
-            type="email" 
-            placeholder="Email..."
-            ref={this.emailRef}
-          />
-          <button
-            onClick={this.onAddFriend}
-            className="add">
+const FriendForm = props => (
+  <div className="form-container">
+    <h2>Add a New Friend</h2>
+    <form className="friends-form">
+      <input
+        name="name"
+        type="text"
+        placeholder="Name..."
+      />
+      <input
+        name="age"
+        type="number"
+        placeholder="Age..."
+      />
+      <input
+        name="email"
+        type="email"
+        placeholder="Email..."
+      />
+      <button
+        type="button"
+        onClick={handleClick(props.addFriend, props.fetchFriends)}
+        className="add"
+      >
             Add Friend
-          </button>
-        </form>
-      </div>
-    );
-  }
-  
-};
+      </button>
+    </form>
+  </div>
+);
 
-const mapStateToProps = state => ({
-  // friends: state.friends,
-  // isFetching: state.isFetching,
-});
-
-export default connect(
-  mapStateToProps,
-  {
-    addFriend,
-  },
-)(FriendForm);
+export default FriendForm;
